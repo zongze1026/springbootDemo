ffac7790df69ddcdcb180e93cb35ee35fd744eb8
xiezongze <xiezz@yitkeji.com>
2018-12-21 10:34:59 +0800

    add log

--- springboot_redis/src/main/java/com/zongze/controller/UserController.java
+++ springboot_redis/src/main/java/com/zongze/controller/UserController.java
@@ -1,6 +1,10 @@
 package com.zongze.controller;
+
+import com.alibaba.fastjson.JSON;
 import com.zongze.model.User;
 import com.zongze.utils.RedisUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,17 +16,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("user")
 public class UserController {
 
+    private static Logger logger = LoggerFactory.getLogger(UserController.class);
 
-    @RequestMapping(value = "add",method = RequestMethod.POST)
-    public String add(){
-        User user = new User("zhangsan",18);
-        RedisUtil.set("user",user,10);
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add() {
+        User user = new User("zhangsan", 18);
+        RedisUtil.set("user", user, 10);
+
+        logger.info("info=" + JSON.toJSONString(user));
+        logger.debug("debug=" + JSON.toJSONString(user));
+        logger.error("error=" + JSON.toJSONString(user));
+        logger.warn("warn=" + JSON.toJSONString(user));
+        logger.trace("trace=" + JSON.toJSONString(user));
         return "success";
     }
-
-
-
-
 
 
 }

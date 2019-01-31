5822c22dfa34ec6e037a22d9f5cccffaf3634dab
xiezongze <xiezz@yitkeji.com>
2019-01-31 11:31:27 +0800

    request

--- springboot_shiro/src/main/java/com/zongze/controller/GlobalExceptionHander.java
+++ springboot_shiro/src/main/java/com/zongze/controller/GlobalExceptionHander.java
@@ -7,8 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import javax.servlet.http.HttpServletRequest;
-
 /**
  * Create By xzz on 2019/1/30
  */
@@ -19,7 +17,7 @@ public class GlobalExceptionHander {
 
 
     @ExceptionHandler(Exception.class)
-    public String hander(HttpServletRequest request,Exception e){
+    public String hander(Exception e){
         logger.error("exception message:{}",e.getMessage());
 
         if(e instanceof UnauthorizedException){

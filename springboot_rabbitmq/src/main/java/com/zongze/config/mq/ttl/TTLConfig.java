18bb17029a55339c9a2e069eb3ac4a8fff7c75cd
xiezongze <xiezz@yitkeji.com>
2019-04-08 17:33:47 +0800

    11

---
+++ springboot_rabbitmq/src/main/java/com/zongze/config/mq/ttl/TTLConfig.java
@@ -0,0 +1,90 @@
+package com.zongze.config.mq.ttl;
+
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Create By xzz on 2019/3/30
+ */
+
+@Configuration
+public class TTLConfig {
+
+    /**
+     * 死信队列
+     */
+    private String ttlqueue = "ttlqueue";
+
+
+    /**
+     * 死信队列
+     */
+    private String test1_ttl = "ttl.test1";
+
+
+    /**
+     * 死信消息转发的交换机
+     */
+    private String ttlExchange = "ttlExchange";
+
+
+    /**
+     * 发送消息到死信队列
+     */
+    private String sendTTLExchange = "sendTTLExchange";
+
+
+    @Bean
+    public Queue ttlTest1() {
+        return new Queue(test1_ttl);
+    }
+
+
+    @Bean
+    public Queue ttlQueue() {
+        Map<String, Object> args = new HashMap<>();
+        /**
+         * 绑定死信交换机
+         */
+        args.put("x-dead-letter-exchange", "ttlExchange");
+        /**
+         * 路由键
+         */
+        args.put("x-dead-letter-routing-key", "ttlkey");
+        /**
+         * 设置消失过期时间(全局消息过期时间)
+         * 如果消息本身带有过期时间的话，那么会以最先过期的时间为准
+         */
+        args.put("x-message-ttl", 30000);
+        return new Queue(ttlqueue, true, false, false, args);
+    }
+
+
+    @Bean
+    public DirectExchange ttlExchange() {
+        return new DirectExchange(ttlExchange);
+    }
+
+
+    @Bean
+    public DirectExchange sendTTLExchange() {
+        return new DirectExchange(sendTTLExchange);
+    }
+
+
+    @Bean
+    public Binding sendExchange() {
+        return BindingBuilder.bind(ttlQueue()).to(sendTTLExchange()).with("ttl.key");
+    }
+
+
+    @Bean
+    public Binding forwordExchange() {
+        return BindingBuilder.bind(ttlTest1()).to(ttlExchange()).with("ttlkey");
+    }
+
+
+}
---
+++ springboot_rabbitmq/src/main/java/com/zongze/send/ttl/TTLSender.java
@@ -0,0 +1,36 @@
+package com.zongze.send.ttl;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.MessageDeliveryMode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * Create By xzz on 2019/3/29
+ */
+@Component
+public class TTLSender {
+
+
+    @Autowired
+    private AmqpTemplate amqpTemplate;
+
+
+    public void send() {
+        String content = "this is topic test %s";
+        System.out.println(String.format(content, "发送成功"));
+        amqpTemplate.convertAndSend("sendTTLExchange", "ttl.key", content, message -> {
+            message.getMessageProperties().setExpiration("500000");
+            return message;
+        });
+    }
+
+
+    public void send1() {
+        String content = "this is topic test %s";
+        System.out.println(String.format(content, "发送成功"));
+        amqpTemplate.convertAndSend("sendTTLExchange", "ttl.key", content);
+    }
+
+
+}

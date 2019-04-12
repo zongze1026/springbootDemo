5dcf5fffa3d735e02fec35a54120a548a7cc842e
xiezongze <xiezz@yitkeji.com>
2019-04-12 10:13:13 +0800

    11

---
+++ springboot_rabbitmq/src/main/java/com/zongze/receice/ttl/TTLMessage2Receive.java
@@ -0,0 +1,31 @@
+package com.zongze.receice.ttl;
+
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * Create By xzz on 2019/3/29
+ */
+@Component
+@RabbitListener(queues = "ttl.test1")
+public class TTLMessage2Receive {
+
+
+    @RabbitHandler
+    public void receive(String content, Channel channel, Message message) throws IOException {
+        System.out.println(TTLMessage2Receive.class.getName() + ":" + content);
+        //当前消息的id
+        long deliveryTag = message.getMessageProperties().getDeliveryTag();
+        //手动应答ack，第二个参数：true:会把小于当前id的消息全部ack掉 false:只ack当前消息
+        System.out.println("===================消息重新入队TTLMessage2Receive=========================");
+//        channel.basicAck(deliveryTag, false);
+        channel.basicReject(deliveryTag,true);
+    }
+
+
+}

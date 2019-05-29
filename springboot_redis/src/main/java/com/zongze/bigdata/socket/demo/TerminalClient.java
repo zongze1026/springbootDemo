d60592a0ffbd98de895e11b003e04285305df664
xiezongze <13916257992@163.com>
2019-05-29 23:02:33 +0800

    aa

---
+++ springboot_redis/src/main/java/com/zongze/bigdata/socket/demo/TerminalClient.java
@@ -0,0 +1,45 @@
+package com.zongze.bigdata.socket.demo;
+
+import jdk.nashorn.internal.runtime.regexp.joni.constants.OPSize;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.Socket;
+
+/**
+ * Created by xieZZ on 2019/5/29
+ */
+public class TerminalClient {
+
+
+    public static void main(String[] args) {
+        OutputStream ops = null;
+        BufferedReader reader = null;
+        try {
+            Socket socket = new Socket("127.0.0.1", 8080);
+            ops = socket.getOutputStream();
+            reader = new BufferedReader(new InputStreamReader(System.in));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                ops.write(line.getBytes("utf-8"));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (ops != null) {
+                    ops.close();
+                }
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}

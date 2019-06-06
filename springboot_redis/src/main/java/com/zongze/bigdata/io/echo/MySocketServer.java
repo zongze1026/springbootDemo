b09a3e93101dc6daf0c41eaefbbba2e088949027
xiezongze <xiezz@yitkeji.com>
2019-06-06 10:39:31 +0800

    过滤器修改

--- springboot_redis/src/main/java/com/zongze/bigdata/io/echo/MySocketServer.java
+++ springboot_redis/src/main/java/com/zongze/bigdata/io/echo/MySocketServer.java
@@ -1,13 +1,7 @@
 package com.zongze.bigdata.io.echo;
-
-import com.zongze.model.User;
-
 import java.io.IOException;
-import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.*;
 
 /**
@@ -16,54 +10,17 @@ import java.util.concurrent.*;
 public class MySocketServer {
 
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         int coreThreadCount = Runtime.getRuntime().availableProcessors();
         ExecutorService executorService = Executors.newScheduledThreadPool(coreThreadCount);
-//        try {
-//            ServerSocket serverSocket = new ServerSocket(8888);
-//            while (true){
-//                Socket clientSocket = serverSocket.accept();
-//                executorService.submit(new SocketWorker(clientSocket));
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
-        List<Callable<User>> tasks = new ArrayList<>();
-        tasks.add(new ThreadPollTest(new User("张三1")));
-        tasks.add(new ThreadPollTest(new User("张三2")));
-        tasks.add(new ThreadPollTest(new User("张三3")));
-        tasks.add(new ThreadPollTest(new User("张三4")));
-        tasks.add(new ThreadPollTest(new User("张三5")));
-        tasks.add(new ThreadPollTest(new User("张三6")));
-        tasks.add(new ThreadPollTest(new User("张三7")));
-        tasks.add(new ThreadPollTest(new User("张三8")));
-        tasks.add(new ThreadPollTest(new User("张三9")));
-        tasks.add(new ThreadPollTest(new User("张三10")));
-        tasks.add(new ThreadPollTest(new User("张三11")));
-        tasks.add(new ThreadPollTest(new User("张三12")));
-        List<Future<User>> futures = executorService.invokeAll(tasks);
-        futures.stream().forEach(future->{
-            try {
-                System.out.println(future.get());
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (ExecutionException e) {
-                e.printStackTrace();
+        try {
+            ServerSocket serverSocket = new ServerSocket(8888);
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                executorService.submit(new SocketWorker(clientSocket));
             }
-        });
-
-
-    }
-
-    private static int getNumber() {
-        int i = 0;
-        for (;;) {
-            System.out.println("线程执行中");
-            if (i == 5) {
-                return i;
-            }
-            i++;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 

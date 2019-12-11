73edf98ac7b98ea7f93215ae3652eb8b95d5ecc6
xiezongze <xiezz@yitkeji.com>
2019-12-11 11:25:27 +0800

    mqSend修改

--- springboot_common/src/main/java/com/zongze/util/EmailUtil.java
+++ springboot_common/src/main/java/com/zongze/util/EmailUtil.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Component;
 
 public class EmailUtil {
 

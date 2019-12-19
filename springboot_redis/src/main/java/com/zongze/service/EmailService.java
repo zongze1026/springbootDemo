f001729967e2617d003a355018bce31f97703631
xiezongze <xiezz@yitkeji.com>
2019-12-19 16:07:19 +0800

    pom修改

--- springboot_redis/src/main/java/com/zongze/service/EmailService.java
+++ springboot_redis/src/main/java/com/zongze/service/EmailService.java
@@ -1,6 +1,7 @@
 package com.zongze.service;
 
 import com.zongze.annotation.Dlock;
+import com.zongze.model.LockType;
 import com.zongze.util.EmailUtil;
 import com.zongze.util.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,12 +26,40 @@ public class EmailService {
         System.out.println(b);
     }
 
-    @Dlock
+    @Dlock(lockType = LockType.Read, name = "com.zongze.service.EmailService")
     public void testLock() throws InterruptedException {
-        System.out.println(Thread.currentThread().getName() + " 抢到了资源锁！");
+        System.out.println(Thread.currentThread().getName() + " 抢到了资源锁！" + " 正在读数据");
         Thread.sleep(1000);
-        System.out.println(Thread.currentThread().getName() + "执行业务完成");
     }
 
 
+    @Dlock(lockType = LockType.Write, name = "com.zongze.service.EmailService")
+    public void testWrite() throws InterruptedException {
+        System.out.println(Thread.currentThread().getName() + " 抢到了资源锁！" + " 正在写数据====================");
+        Thread.sleep(5000);
+        System.out.println("写线程已经处理完");
+    }
+
+
+    @Dlock(lockType = LockType.Write, name = "com.zongze.service.EmailService")
+    public void write() throws InterruptedException {
+        System.out.println(Thread.currentThread().getName() + " 抢到了资源锁！" + " 正在写数据===========");
+        Thread.sleep(1000);
+        System.out.println("写线程已经处理完");
+        read();
+    }
+
+    @Dlock(lockType = LockType.Read, name = "com.zongze.service.EmailService")
+    public void read() throws InterruptedException {
+        System.out.println(Thread.currentThread().getName() + " 抢到了资源锁！" + " 正在读数据====================");
+        Thread.sleep(1000);
+        System.out.println("线程处理完读数据");
+    }
+
+    @Dlock(lockType = LockType.Write, name = "com.zongze.service.EmailService")
+    public void WriteReentrant() throws InterruptedException {
+        System.out.println(Thread.currentThread().getName() + " 抢到了资源锁！" + " 正在处理第一个写方法");
+        Thread.sleep(1000);
+        write();
+    }
 }

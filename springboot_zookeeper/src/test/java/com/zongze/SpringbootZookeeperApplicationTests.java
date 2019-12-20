be6ce3fcd17ed47782010810716e8a4adfc5b82d
xiezongze <xiezz@yitkeji.com>
2019-12-20 18:30:39 +0800

    zk

---
+++ springboot_zookeeper/src/test/java/com/zongze/SpringbootZookeeperApplicationTests.java
@@ -0,0 +1,11 @@
+package com.zongze;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class SpringbootZookeeperApplicationTests {
+
+
+}

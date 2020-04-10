599de7cf09ee82f29e296124faf71879f4be0481
xiezongze <xiezz@yitkeji.com>
2020-04-10 17:46:29 +0800

    a

--- springboot_common/src/main/java/com/zongze/util/ObjectUtil.java
+++ springboot_common/src/main/java/com/zongze/util/ObjectUtil.java
@@ -190,6 +190,21 @@ public class ObjectUtil {
     }
 
 
+    public static byte[] int2bytes(int i) {
+        byte[] bytes = new byte[4];
+        bytes[0] = (byte) (i >> 24);
+        bytes[1] = (byte) (i >> 16);
+        bytes[2] = (byte) (i >> 8);
+        bytes[3] = (byte) (i >> 0);
+        return bytes;
+    }
+
+
+    public static int bytes2int(byte[] bytes) {
+        return (bytes[0] << 24) | ((bytes[1] & 0xff) << 16) | ((bytes[2] & 0xff) << 8) | (bytes[3] & 0xff);
+    }
+
+
 
 
 

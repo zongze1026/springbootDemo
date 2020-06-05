b51dcdc50a509049fbbdfe81b15e3a43c14d29a8
xiezongze <xiezz@yitkeji.com>
2020-06-05 15:50:27 +0800

    httputil

--- springboot_common/src/main/java/com/zongze/util/HttpUtil.java
+++ springboot_common/src/main/java/com/zongze/util/HttpUtil.java
@@ -1,73 +1,150 @@
 package com.zongze.util;
-
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.commons.httpclient.methods.GetMethod;
-import org.apache.commons.httpclient.methods.PostMethod;
-import org.apache.commons.httpclient.methods.RequestEntity;
-import org.apache.commons.httpclient.methods.StringRequestEntity;
-
-import java.io.BufferedReader;
+import org.apache.http.*;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.config.ConnectionConfig;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.nio.charset.CodingErrorAction;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+/**
+ * Create By xzz on 2020/6/5
+ */
 public class HttpUtil {
 
-    //post发送json
-    public static String postForJson(String url, String jsonData) {
-        HttpClient httpClient = new HttpClient();
-        PostMethod post = null;
+
+    private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36";
+
+    // 超时设置
+    private static final RequestConfig requestConfig = RequestConfig.custom()
+            .setConnectTimeout(5000)
+            .setConnectionRequestTimeout(5000)
+            .setSocketTimeout(10000)
+            .build();
+
+    // 编码设置
+    private static final ConnectionConfig connectionConfig = ConnectionConfig.custom()
+            .setMalformedInputAction(CodingErrorAction.IGNORE)
+            .setUnmappableInputAction(CodingErrorAction.IGNORE)
+            .setCharset(Consts.UTF_8)
+            .build();
+
+    private static HttpClientBuilder getBuilder() {
+        List<Header> headers = new ArrayList<>();
+        Header header = new BasicHeader("User-Agent", USER_AGENT);
+        headers.add(header);
+        return HttpClients.custom().setDefaultConnectionConfig(connectionConfig).setDefaultHeaders(headers).setDefaultRequestConfig(requestConfig);
+    }
+
+    /**
+     * 发送HttpGet请求
+     *
+     * @param url 请求地址
+     * @return
+     */
+    public static String sendGet(String url) {
+        CloseableHttpClient httpclient = getBuilder().build();
+        HttpGet httpGet = new HttpGet(url);
+        String result = null;
         try {
-            post = new PostMethod(url);
-            RequestEntity se = new StringRequestEntity(jsonData, "application/json", "utf-8");
-            post.setRequestEntity(se);
-            int statusCode = httpClient.executeMethod(post);
-            if (statusCode != HttpStatus.SC_OK) {
-                System.out.println("请求失败,响应状态码:" + statusCode);
-            } else {
-                return parseResult(post.getResponseBodyAsStream());
-            }
-        } catch (Exception e) {
+            CloseableHttpResponse response = httpclient.execute(httpGet);
+            HttpEntity httpEntity = response.getEntity();
+            result = EntityUtils.toString(httpEntity);
+        } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            if (post != null)
-                post.releaseConnection();
         }
-        return null;
+        return result;
     }
 
-
-    //get请求
-    public static String get(String url) {
-        HttpClient httpClient = new HttpClient();
-        GetMethod get = null;
+    /**
+     * 发送HttpPost请求，参数为json字符串
+     *
+     * @param url     请求地址
+     * @param jsonStr json字符串
+     * @return
+     */
+    public static String sendPost(String url, String jsonStr) {
+        CloseableHttpClient httpclient = getBuilder().build();
+        HttpPost httpPost = new HttpPost(url);
+        String result = null;
         try {
-            get = new GetMethod(url);
-            int statusCode = httpClient.executeMethod(get);
-            if (statusCode != HttpStatus.SC_OK) {
-                System.out.println("请求失败,响应状态码:" + statusCode);
-            } else {
-                return parseResult(get.getResponseBodyAsStream());
-            }
-        } catch (Exception e) {
+            StringEntity stringEntity = new StringEntity(jsonStr, Consts.UTF_8);
+            stringEntity.setContentType("application/json");
+            httpPost.setEntity(stringEntity);
+            CloseableHttpResponse httpResponse = httpclient.execute(httpPost);
+            HttpEntity httpEntity = httpResponse.getEntity();
+            result = EntityUtils.toString(httpEntity);
+        } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            if (get != null)
-                get.releaseConnection();
         }
-        return null;
+        return result;
+
     }
 
-    private static String parseResult(InputStream in) throws IOException {
-        StringBuffer resultBuffer = new StringBuffer();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, "utf-8"));
-        String line;
-        while ((line = bufferedReader.readLine()) != null) {
-            resultBuffer.append(line);
+
+
+    /**
+     * 提交表单
+     * @param url:地址
+     * @param headers:需要设置的请求头
+     * @param body:表单内容
+     */
+    public static String sendForm(String url, Map<String, String> headers, Map<String, String> body) {
+        CloseableHttpClient httpclient = getBuilder().build();
+        HttpPost httpPost = new HttpPost(url);
+        String result = null;
+        try {
+            headers.entrySet().stream().forEach(entry -> httpPost.setHeader(entry.getKey(), entry.getValue()));
+            if (!body.isEmpty()) {
+                List<NameValuePair> nameValuePairList = new ArrayList<>();
+                for (String key : body.keySet()) {
+                    nameValuePairList.add(new BasicNameValuePair(key, body.get(key)));
+                }
+                UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(nameValuePairList, Consts.UTF_8);
+                formEntity.setContentType("application/x-www-form-urlencoded; charset=UTF-8");
+                httpPost.setEntity(formEntity);
+                HttpResponse response = httpclient.execute(httpPost);
+                result = EntityUtils.toString(response.getEntity());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return resultBuffer.toString();
+        return result;
     }
 
+
+    public static void main(String[] args) throws IOException {
+        Map<String, String> headers = new HashMap();
+        headers.put("Authorization", "APPCODE 86d51f5a987c4c6382389f7ac3d745f1");
+        headers.put("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        String url = "http://yunyidata.market.alicloudapi.com/bankAuthenticate4";
+        Map<String, String> body = new HashMap<>();
+        body.put("ReturnBankInfo", "YES");
+        body.put("cardNo", "6222081204001911076");
+        body.put("idNo", "330481198910010033");
+        body.put("name", "王占宇");
+        body.put("phoneNo", "13967337731");
+        String s = sendForm(url, headers, body);
+        System.out.println(s);
+
+    }
 
 }
+
+
+
+
+
---
+++ springboot_redis/src/main/java/com/zongze/bigdata/socket/demo/ConvertUtil.java
@@ -0,0 +1,50 @@
+package com.zongze.bigdata.socket.demo;
+
+/**
+ * Create By xzz on 2020/5/12
+ */
+public class ConvertUtil {
+
+
+    private static final char[] HEX_CHAR = {'0', '1', '2', '3', '4', '5',
+            '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+    /**
+     * byte[] to hex string
+     *
+     * @param bytes
+     * @return
+     */
+    public static String bytesToHexString(byte[] bytes) {
+        char[] buf = new char[bytes.length * 2];
+        int index = 0;
+        for (byte b : bytes) { // 利用位运算进行转换，可以看作方法一的变种
+            buf[index++] = HEX_CHAR[b >>> 4 & 0xf];
+            buf[index++] = HEX_CHAR[b & 0xf];
+        }
+
+        return new String(buf);
+    }
+
+
+    /**
+     * 将16进制字符串转换为byte[]
+     *
+     * @param str
+     * @return
+     */
+    public static byte[] hexStringToBytes(String str) {
+        if (str == null || str.trim().equals("")) {
+            return new byte[0];
+        }
+
+        byte[] bytes = new byte[str.length() / 2];
+        for (int i = 0; i < str.length() / 2; i++) {
+            String subStr = str.substring(i * 2, i * 2 + 2);
+            bytes[i] = (byte) Integer.parseInt(subStr, 16);
+        }
+
+        return bytes;
+    }
+
+}

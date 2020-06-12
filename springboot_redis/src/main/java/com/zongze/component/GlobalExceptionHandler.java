955e02d2aedf1bc8be5b2f0a02ee7d54d232cc8f
xiezongze <xiezz@yitkeji.com>
2020-06-12 16:20:59 +0800

    swagger bug

--- springboot_redis/src/main/java/com/zongze/component/GlobalExceptionHandler.java
+++ springboot_redis/src/main/java/com/zongze/component/GlobalExceptionHandler.java
@@ -1,17 +1,14 @@
 package com.zongze.component;
-
-import com.zongze.entity.ExceptionEnum;
-import com.zongze.entity.ResultResp;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+
 
 /**
  * Create By xzz on 2019/3/27
@@ -21,23 +18,30 @@ import java.util.List;
 @ResponseBody
 public class GlobalExceptionHandler {
 
-    private static Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-
-
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResultResp handleIllegalParamException(MethodArgumentNotValidException e) {
+    public String handleIllegalParamException(MethodArgumentNotValidException e) {
         List<ObjectError> errors = e.getBindingResult().getAllErrors();
         String tips = "参数不合法";
         if (errors.size() > 0) {
             tips = errors.get(0).getDefaultMessage();
         }
-        return ResultResp.fail(tips);
+        return tips;
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public String handlerNullParamException(HttpMessageNotReadableException e) {
+        return "请求参数不能为空";
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public String handlerNullParamException(HttpRequestMethodNotSupportedException e) {
+        return "不允许"+"["+e.getMethod()+"]请求";
     }
 
 
     @ExceptionHandler(Exception.class)
-    public ResultResp exceptionHandler(HttpServletRequest request, Exception e) {
-        logger.info("请求服务器异常", e);
-        return ResultResp.fail(ExceptionEnum.SYSTEM_ERROR);
+    public String exceptionHandler(HttpServletRequest request, Exception e) {
+        e.printStackTrace();
+        return "服务器异常";
     }
 }

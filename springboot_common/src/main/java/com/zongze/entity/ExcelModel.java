00a15759853d319f201a7be4ac27857d9c94be78
xiezongze <xiezz@haibaobaoxian.cn>
2020-08-18 09:20:57 +0800

    'a'

---
+++ springboot_common/src/main/java/com/zongze/entity/ExcelModel.java
@@ -0,0 +1,17 @@
+package com.zongze.entity;
+
+/**
+ * @Date 2020/8/17 13:34
+ * @Created by xzz
+ */
+public interface ExcelModel {
+
+
+    public static void main(String[] args) {
+        System.out.println(int.class == Integer.class);
+    }
+
+
+
+
+}

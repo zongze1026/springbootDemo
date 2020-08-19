7bf72265cd9882ce74fcd02387dce15056026db9
xiezongze <xiezz@haibaobaoxian.cn>
2020-08-19 22:38:32 +0800

    a

---
+++ springboot_mybatis/src/main/java/com/zongze/controller/aaa.java
@@ -0,0 +1,14 @@
+package com.zongze.controller;
+
+/**
+ * @Date 2020/8/19 22:34
+ * @Created by xzz
+ */
+public class aaa {
+
+    public static void main(String[] args) {
+        System.out.println("aaaa");
+    }
+
+
+}

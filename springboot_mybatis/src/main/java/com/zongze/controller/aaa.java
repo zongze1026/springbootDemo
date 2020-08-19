bac404c6ded6ff94c9e557499d288a7fcb99cf32
xiezongze <xiezz@haibaobaoxian.cn>
2020-08-19 22:57:14 +0800

    'a'

---
+++ springboot_mybatis/src/main/java/com/zongze/controller/aaa.java
@@ -0,0 +1,13 @@
+package com.zongze.controller;
+
+import java.sql.SQLOutput;
+
+/**
+ * @Date 2020/8/19 22:52
+ * @Created by xzz
+ */
+public class aaa {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}

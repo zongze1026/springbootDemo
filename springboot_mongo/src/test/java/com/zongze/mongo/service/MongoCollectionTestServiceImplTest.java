0918dbfc3b8ed37840fb945ee78a23138c7c73e4
xiezongze <13916257992@163.com>
2021-01-21 09:11:52 +0800

    netty heart

--- springboot_mongo/src/test/java/com/zongze/mongo/service/MongoCollectionTestServiceImplTest.java
+++ springboot_mongo/src/test/java/com/zongze/mongo/service/MongoCollectionTestServiceImplTest.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.sql.SQLOutput;
 import java.util.List;
 
 

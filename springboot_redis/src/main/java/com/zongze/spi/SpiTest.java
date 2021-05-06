9b04eb0e029d1a523245fc821bd69ff0fbbefa39
xiezongze <13916257992@163.com>
2021-05-06 11:20:49 +0800

    spi

--- springboot_redis/src/main/java/com/zongze/spi/SpiTest.java
+++ springboot_redis/src/main/java/com/zongze/spi/SpiTest.java
@@ -1,6 +1,8 @@
 package com.zongze.spi;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.ServiceLoader;
 
 /**
  * @Date 2021/4/25 14:42
@@ -10,12 +12,12 @@ public class SpiTest {
 
 
     public static void main(String[] args) {
-//        ServiceLoader<Search> load = ServiceLoader.load(Search.class);
-//        Iterator<Search> iterator = load.iterator();
-//        while (iterator.hasNext()){
-//            Search next = iterator.next();
-//            next.searchDoc("美女");
-//        }
+        ServiceLoader<Search> load = ServiceLoader.load(Search.class);
+        Iterator<Search> iterator = load.iterator();
+        while (iterator.hasNext()){
+            Search next = iterator.next();
+            next.searchDoc("美女");
+        }
 
 
     }
